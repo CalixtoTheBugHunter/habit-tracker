28042debeff5660c9c49aaf75b732a7576c160b8
Paulo Calixto <paulo.calixto@onepeloton.com>
2025-11-28 19:53:30 -0300

    feat(ui): add offline indicator component with wifi icon (#57)

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default [
         document: 'readonly',
         console: 'readonly',
         global: 'readonly',
+        navigator: 'readonly',
         DOMException: 'readonly',
         setTimeout: 'readonly',
         clearTimeout: 'readonly',
